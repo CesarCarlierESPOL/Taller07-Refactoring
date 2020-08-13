24f692a1c83a19dd1cb5fffcf534ca0a13f234ea
César Carlier <ccarlier@espol.edu.ec>
2020-08-13 15:14:18 -0500

    Refactorizacion de 2 code smells
    Refactorizando: Se elimino parametro inutilizado en Profesor y se elimino Lazy Class calcularSueldoProfesor, refactorizando sus metodos a la clase Profesor

--- TalllerRefactoring/src/modelos/Profesor.java
+++ TalllerRefactoring/src/modelos/Profesor.java
@@ -12,7 +12,7 @@ public class Profesor {
     public InformacionAdicionalProfesor info;
     public ArrayList<Paralelo> paralelos;
 
-    public Profesor(String codigo, String nombre, String apellido, String facultad, int edad, String direccion, String telefono) {
+    public Profesor(String codigo, String nombre, String apellido, int edad, String direccion, String telefono) {
         this.codigo = codigo;
         this.nombre = nombre;
         this.apellido = apellido;
@@ -24,6 +24,12 @@ public class Profesor {
     
     public void anadirParalelos(Paralelo p){
         paralelos.add(p);
+    }
+    
+    public double calcularSueldo(){
+        double sueldo=0;
+        sueldo= info.añosdeTrabajo*600 + info.BonoFijo;
+        return sueldo;
     }
     
     
